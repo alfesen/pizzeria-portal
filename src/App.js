789552f5bbd9d34f6cbd3e08bfcbe0a7af35a89c
alfesen <89510380+alfesen@users.noreply.github.com>
2022-01-08 19:00:28 +0100

    Import views, apply Routing

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import MainLayout from './components/layout/MainLayout/MainLayout';
+import Dashboard from './components/views/Dashboard/Dashboard'
+import Login from './components/views/Login/Login';
+import Tables from './components/views/Tables/Tables';
+import Waiter from './components/views/Waiter/Waiter';
+import Kitchen from './components/views/Kitchen/Kitchen';
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <BrowserRouter basename={'/panel'}>
+      <MainLayout>
+        <Routes>
+          <Route exact path={`${process.env.PUBLIC_URL}/`} component={Dashboard} />
+          <Route exact path={process.env.PUBLIC_URL + '/login'} component={Login} />
+          <Route exact path={process.env.PUBLIC_URL + '/tables'} component={Tables} />
+          <Route exact path={process.env.PUBLIC_URL + '/waiter'} component={Waiter} />
+          <Route exact path={process.env.PUBLIC_URL + '/kitchen'} component={Kitchen} />
+        </Routes>
+      </MainLayout>
+    </BrowserRouter>
+  )
+};
 
 export default App;
