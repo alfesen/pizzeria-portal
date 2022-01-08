07c8689bc6158b7f3e7e8b49a737a3f9f51ddf64
alfesen <89510380+alfesen@users.noreply.github.com>
2022-01-08 18:39:06 +0100

    Add views with postcss problem

---
+++ src/components/views/Waiter/Waiter.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Waiter.module.scss';
+
+const Waiter = () => {
+  return(
+    <div className={styles.component}>
+      <h2>Waiter</h2>
+    </div>
+  );
+};
+
+export default Waiter;
