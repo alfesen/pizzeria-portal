79fd37cb8be9d97054ada90f04a03669f8b82df8
alfesen <89510380+alfesen@users.noreply.github.com>
2022-01-30 18:11:57 +0100

    Create Login view

--- src/components/views/Login/Login.js
+++ src/components/views/Login/Login.js
@@ -1,12 +1,34 @@
 import React from 'react';
 import styles from './Login.module.scss';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Button from '@material-ui/core/Button';
+import {NavLink} from 'react-router-dom';
 
 const Login = () => {
   return (
-    <div className={styles.component}>
-      <h2>Log in</h2>
+    <div>
+      <div className={styles.component}>
+        <h2>Log in</h2>
+      </div>
+
+      <div className={styles.component}>
+        <Box component='form' sx={{
+          '& .MuiTextField-root': { m: 1, width: '25ch' },
+        }}
+        noValidate
+        autoComplete="off">
+          <div>
+            <TextField requiered  label='Login name' />
+          </div>
+          <div>
+            <TextField requiered label='Password' type='password' />
+          </div>
+          <Button className={styles.submit} to={'/panel'} component={NavLink}>Submit</Button>
+        </Box>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Login;
