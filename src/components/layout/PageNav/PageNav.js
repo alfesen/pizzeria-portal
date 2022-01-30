4b0b8fc242858f83e1d3b3a9b5bd32115c527da3
alfesen <89510380+alfesen@users.noreply.github.com>
2022-01-30 17:45:53 +0100

    Create Page Navigation with MUI

---
+++ src/components/layout/PageNav/PageNav.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import styles from './PageNav.module.scss';
+import Button from '@material-ui/core/Button';
+
+const PageNav = () => {
+
+  return (
+    <nav className={styles.component}>
+      <Button className={styles.link} component={NavLink} exact to={`${process.env.PUBLIC_URL}/`} activeClassName='active'>Home</Button>
+      <Button className={styles.link} component={NavLink} to={`${process.env.PUBLIC_URL}/login`} activeClassName='active'>Login</Button>
+      <Button className={styles.link} component={NavLink} to={`${process.env.PUBLIC_URL}/tables`} activeClassName='active'>Tables</Button>
+      <Button className={styles.link} component={NavLink} to={`${process.env.PUBLIC_URL}/waiter`} activeClassName='active'>Waiter</Button>
+      <Button className={styles.link} component={NavLink} to={`${process.env.PUBLIC_URL}/kitchen`} activeClassName='active'>Kitchen</Button>
+    </nav>
+  );
+};
+
+export default PageNav;
